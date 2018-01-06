5ae8bd1599dbfe25ab836105c1082a621c98b94f
Shubhangi Bharti <shubhangibharti@yahoo.co.in>
2018-01-06 21:28:52 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$conn=mysqli_connect("hostname","username","password","databasename");
-if(!$conn) {
-        die('Could not connect: ' . mysql_error());
-    }
-    mysqli_select_db($conn, "databasename");
-?>
